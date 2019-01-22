e5300b89dba9b75a5f9e156ad6900626b08debcb
Varun Goyal <42479771+govarun@users.noreply.github.com>
2019-01-22 21:05:57 +0530

    Create hello-world.py

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+print("Hello World")
+
+print("You have reached govarun's account")
+
+print("It is a pleasure to see you visiting my page")
+print("But how will I know that you visited my page", end = '. ')
+print("Let me think..")
